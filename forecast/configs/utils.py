ff8d721a59977ea09a60da621ce20f46373bf361
georgesmiaka <miakageorge@yahoo.fr>
2024-05-19 01:34:43 +0200

    ITPF v.1.0.0

--- forecast/configs/utils.py
+++ forecast/configs/utils.py
@@ -1,161 +1,104 @@
-import numpy as np
-import tensorflow as tf
+from pathlib import Path
 import matplotlib.pyplot as plt
-
-class WindowGenerator():
-    def __init__(self, input_width, label_width, shift,
-                train_df, val_df, test_df,
-                label_columns=None):
-        # Store the raw data.
-        self.train_df = train_df
-        self.val_df = val_df
-        self.test_df = test_df
-
-        # Work out the label column indices.
-        self.label_columns = label_columns
-        if label_columns is not None:
-            self.label_columns_indices = {name: i for i, name in
-                                        enumerate(label_columns)}
-        self.column_indices = {name: i for i, name in
-                            enumerate(train_df.columns)}
-
-        # Work out the window parameters.
-        self.input_width = input_width
-        self.label_width = label_width
-        self.shift = shift
-
-        self.total_window_size = input_width + shift
-
-        self.input_slice = slice(0, input_width)
-        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
-
-        self.label_start = self.total_window_size - self.label_width
-        self.labels_slice = slice(self.label_start, None)
-        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
-
-    def __repr__(self):
-        return '\n'.join([
-            f'Total window size: {self.total_window_size}',
-            f'Input indices: {self.input_indices}',
-            f'Label indices: {self.label_indices}',
-            f'Label column name(s): {self.label_columns}'])
-  
-    def split_window(self, features):
-        inputs = features[:, self.input_slice, :]
-        labels = features[:, self.labels_slice, :]
-        if self.label_columns is not None:
-            labels = tf.stack(
-                [labels[:, :, self.column_indices[name]] for name in self.label_columns],
-                axis=-1)
-
-        # Slicing doesn't preserve static shape information, so set the shapes
-        # manually. This way the `tf.data.Datasets` are easier to inspect.
-        inputs.set_shape([None, self.input_width, None])
-        labels.set_shape([None, self.label_width, None])
-
-        return inputs, labels
-  
-    def make_dataset(self, data):
-        data = np.array(data, dtype=np.float32)
-        ds = tf.keras.utils.timeseries_dataset_from_array(
-            data=data,
-            targets=None,
-            sequence_length=self.total_window_size,
-            sequence_stride=1,
-            shuffle=True,
-            batch_size=32,)
-
-        ds = ds.map(self.split_window)
-
-        return ds
-    
-    def plot(self, model=None, plot_col='pos.x', max_subplots=3):
-        inputs, labels = self.example
-        plt.figure(figsize=(12, 8))
-        plot_col_index = self.column_indices[plot_col]
-        max_n = min(max_subplots, len(inputs))
-        for n in range(max_n):
-            plt.subplot(max_n, 1, n+1)
-            plt.ylabel(f'{plot_col} [normed]')
-            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
-                    label='Inputs', marker='.', zorder=-10)
-
-            if self.label_columns:
-                label_col_index = self.label_columns_indices.get(plot_col, None)
-            else:
-                label_col_index = plot_col_index
-
-            if label_col_index is None:
-                continue
-
-            plt.scatter(self.label_indices, labels[n, :, label_col_index],
-                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
-            if model is not None:
-                predictions = model(inputs)
-                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
-                        marker='X', edgecolors='k', label='Predictions',
-                        c='#ff7f0e', s=64)
-
-            if n == 0:
-                plt.legend()
-
-        plt.xlabel('Minute [s]')
-    
-    def plot2D(self, model=None, plot_col='pos.x', plot_col2='pos.y', max_subplots=3):
-        inputs, labels = self.example
-        plt.figure(figsize=(12, 8))
-        plot_col_index = self.column_indices[plot_col]
-        plot_col_index2 = self.column_indices[plot_col2]
-        max_n = min(max_subplots, len(inputs))
-        for n in range(max_n):
-            plt.subplot(max_n, 1, n+1)
-            plt.ylabel('pos(x, y)')
-            plt.plot(self.input_indices, inputs[n, :, plot_col_index],  inputs[n, :, plot_col_index2],
-                    label='Inputs', marker='.', zorder=-10)
-
-            if self.label_columns:
-                label_col_index = self.label_columns_indices.get(plot_col, None)
-                label_col_index2 = self.label_columns_indices.get(plot_col2, None)
-            else:
-                label_col_index = plot_col_index
-                label_col_index2 = plot_col_index2
-
-            if label_col_index is None:
-                continue
-
-            plt.scatter(labels[n, :, label_col_index], labels[n, :, label_col_index2],
-                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
-            if model is not None:
-                predictions = model(inputs)
-                plt.scatter(self.label_indices, predictions[n, :, label_col_index],  predictions[n, :, label_col_index2],
-                        marker='X', edgecolors='k', label='Predictions',
-                        c='#ff7f0e', s=64)
-
-            if n == 0:
-                plt.legend()
-
-        plt.xlabel('Minute [s]')
+import numpy as np
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error
 
 
-    @property
-    def train(self):
-        return self.make_dataset(self.train_df)
 
-    @property
-    def val(self):
-        return self.make_dataset(self.val_df)
 
-    @property
-    def test(self):
-        return self.make_dataset(self.test_df)
+def split_sequence(
+    sequence: np.ndarray, ratio: float = 0.99
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Splits a sequence into 2 (3) parts, as is required by our transformer
+    model.
 
-    @property
-    def example(self):
-        """Get and cache an example batch of `inputs, labels` for plotting."""
-        result = getattr(self, '_example', None)
-        if result is None:
-            # No example batch was found, so get one from the `.train` dataset
-            result = next(iter(self.train))
-            # And cache it for next time
-            self._example = result
-        return result
+    Assume our sequence length is L, we then split this into src of length N
+    and tgt_y of length M, with N + M = L.
+    src, the first part of the input sequence, is the input to the encoder, and we
+    expect the decoder to predict tgt_y, the second part of the input sequence.
+    In addition we generate tgt, which is tgt_y but "shifted left" by one - i.e. it
+    starts with the last token of src, and ends with the second-last token in tgt_y.
+    This sequence will be the input to the decoder.
+
+
+    Args:
+        sequence: batched input sequences to split [bs, seq_len, num_features]
+        ratio: split ratio, N = ratio * L
+
+    Returns:
+        tuple[torch.Tensor, torch.Tensor, torch.Tensor]: src, tgt, tgt_y
+    """
+    src_end = int(sequence.shape[1] * ratio)
+    # [bs, src_seq_len, num_features]
+    src = sequence[:, :src_end]
+    # [bs, tgt_seq_len, num_features]
+    tgt = sequence[:, src_end - 1 : -1]
+    # [bs, tgt_seq_len, num_features]
+    tgt_y = sequence[:, src_end:]
+
+    return src, tgt, tgt_y
+
+def split_sequence_long(
+    sequence: np.ndarray, ratio: float = 0.8
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Splits a sequence into 2 (3) parts, as is required by our transformer
+    model.
+
+    Assume our sequence length is L, we then split this into src of length N
+    and tgt_y of length M, with N + M = L.
+    src, the first part of the input sequence, is the input to the encoder, and we
+    expect the decoder to predict tgt_y, the second part of the input sequence.
+    In addition we generate tgt, which is tgt_y but "shifted left" by one - i.e. it
+    starts with the last token of src, and ends with the second-last token in tgt_y.
+    This sequence will be the input to the decoder.
+
+
+    Args:
+        sequence: batched input sequences to split [bs, seq_len, num_features]
+        ratio: split ratio, N = ratio * L
+
+    Returns:
+        tuple[torch.Tensor, torch.Tensor, torch.Tensor]: src, tgt, tgt_y
+    """
+    src_end = int(sequence.shape[1] * ratio)
+    # [bs, src_seq_len, num_features]
+    src = sequence[:, :src_end]
+    # [bs, tgt_seq_len, num_features]
+    tgt = sequence[:, src_end - 1 : -1]
+    # [bs, tgt_seq_len, num_features]
+    tgt_y = sequence[:, src_end:]
+
+    return src, tgt, tgt_y
+
+
+def windowing(data, chunk_size):
+    # Initialize an empty list to store the grouped time series data
+    grouped_data = []
+    for start in range(0, len(data), chunk_size):
+        end = start + chunk_size
+        # Extract the chunk of data for this group
+        chunk = data.iloc[start:end]
+        # Convert the chunk to a list of lists (each sublist contains heading, posx, posy, posz, etc...)
+        hxyz = chunk.values.tolist()
+        # Append this chunk to the grouped_data list
+        grouped_data.append(hxyz)
+    grouped_data_np = np.array(grouped_data, dtype=float)
+    return grouped_data_np
+
+def windowing_array(data, chunk_size):
+    # Initialize an empty list to store the grouped time series data
+    grouped_data = []
+    for start in range(0, len(data), chunk_size):
+        end = start + chunk_size
+        # Extract the chunk of data for this group
+        chunk = data[start:end]
+        # Convert the chunk to a list of lists (each sublist contains heading, posx, posy, posz, etc...)
+        hxyz = chunk.values.tolist()
+        # Append this chunk to the grouped_data list
+        grouped_data.append(hxyz)
+    grouped_data_np = np.array(grouped_data, dtype=float)
+    return grouped_data_np
+
--- forecast/explainers/shap.py
+++ forecast/explainers/shap.py
@@ -1,147 +1,68 @@
-from abc import ABCMeta, abstractmethod
-from scipy.special import comb
 import math
 import numpy as np
 import pandas as pd
 from itertools import product
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
 
 
-class BlackBox:
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def predict(self, X):
-        return
-
-    @abstractmethod
-    def predict_proba(self, X):
-        return
-    
-    @abstractmethod
-    def predict_2Dto3D(self, X):
-        return
-    
-class Explainer:
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def fit_exp(self, model, data, features_name):
-        return
-    
-    @abstractmethod
-    def shap_values_multivariate(self, X):
-        return
-    
-    @abstractmethod
-    def shap_multivariate(self, X):
-        return
-
-
-class BlackBoxWrapper(BlackBox):
-    def __init__(self, model):
-        self.clf = model
-
-    def predict(self, X):
-        y = self.clf.predict(X)
-        return y
-    
-    def predict_proba(self, X):
-        y = self.clf.predict_proba(X)
-        return y
-    
-    def predict_2Dto3D(self, X):
-        y = transform_to_3d(X)
-        x = self.predict(y)
-        return x
-    
-## 
-#   Data wrapper - functions
-##
-    
-def is_3d(data):
-    y = False
-    if data.ndim == 3:
-        y = True
-    return y
-
-def is_2d(data):
-    y = False
-    if data.ndim == 2:
-        y = True
-    return y
-
-def transform_to_3d(data):
-    y = data.reshape((1, data.shape[0], data.shape[1])) #(num_samples, sample_size, nu_features)
-    return y
-
-def transform_to_2d(data):
-    y = data.reshape(data.shape[0]*data.shape[1], data.shape[2])
-    return y
-
-
-class ITPFExplainer(Explainer):
+class SHAP():
     '''
-    The Explainer class.
-
+    The SHAP Explainer class.
+    
     Parameters:
-    model: model or prediction function of model.
-    data_x: Data used to initialize the explainers with, (3D shape).
-    data_y: Data used to explain the model, (2D shape).
-    features-names: List of features names.
-    class-names: List of the class name to be explained.
+    model: model or prediction function of model takes 2D input (np.ndarray) and return 2D output.
+    features_list_names: List of name of features.
+    labels_name: List of the labels being predicted.
+    loss: loss function for measuring the prediction accurancy
     '''
     def __init__(self):
         self.model = None
-        self.data_x = None
-        self.data_y = None
-        self.feature_names = None
-        self.class_name = None
-        self.feature_num = None
-        self.feature_pred_num = None
+        self.feature_list_names = None
+        self.labels_name = None
+        self.loss = None
 
-    def fit_exp(self, model, x, y, feature_names, class_names, feature_nr, feature_pred_nr):
+    def fit_exp(self, model, loss, features_list_names, labels_name):
         self.model = model
-        self.data_x = x
-        self.data_y = y
-        self.feature_names = feature_names
-        self.class_names = class_names
-        self.feature_num = feature_nr
-        self.feature_pred_num = feature_pred_nr
+        self.loss = loss
+        self.feature_names = features_list_names
+        self.class_names = labels_name
     
+    def _is_3d(self, data):
+        y = False
+        if data.ndim == 3:
+            y = True
+        return y
 
-    def _generate_perturbed_samples(self, original_sequence, scale=3.5,):
+    def _is_2d(self, data):
+        y = False
+        if data.ndim == 2:
+            y = True
+        return y
+
+    def _transform_to_3d(self, data):
+        y = data.reshape((1, data.shape[0], data.shape[1])) #(num_samples, sample_size, nu_features)
+        return y
+
+    def _transform_to_2d(self, data):
+        y = data.reshape(data.shape[0]*data.shape[1], data.shape[2])
+        return y
+
+    def _perturb_time_series(self, matrix):
         """
-        Generates perturbed samples that are significantly different from the original sequence.
+        Generates a signle perturbed sample that is significantly different from the original sequence.
+        The algorithm starts by computing the correlation matrix of the input data matrix using Pearson 
+        correlation coefficient. Then, it iterates over each feature in the matrix and identifies 
+        highly correlated features (correlation coefficient between 0.7 and 1.0). For each highly 
+        correlated feature, it computes a perturbation value based on the correlation grade between 
+        the features. However, for features that are not highly correlated with any other feature, 
+        noise perturbation is added to introduce variation in their values.
         
         Parameters:
-            original_sequence (np.ndarray): The original sequence of shape (timesteps, features).
-            scale (float): Scale of perturbation. For 'range_based', it's a percentage of the feature range.
-                        For 'fixed_noise', it's the magnitude of noise added directly.
+            matrix (np.ndarray): The original sequence of shape (timesteps, features).
         
         Returns:
             np.ndarray: A single perturbed sample of the same shape as the original sequence.
         """
-        # Ensure the input is a NumPy array for manipulation
-        original_sequence = np.array(original_sequence)
-
-        # Initialize an array to hold the perturbed samples
-        perturbed_samples = np.zeros((original_sequence.shape[0], original_sequence.shape[1]))
-
-        # Calculate the standard deviation of the original sequence
-        std_devs = np.std(original_sequence, axis=0)
-
-        # Generate noise
-        perturbation = (std_devs + scale) * (original_sequence[1])
-        
-        # Generate the perturbed sample
-        perturbed_samples = original_sequence + perturbation
-        
-        return perturbed_samples
-    
-    def _perturb_time_series(self, matrix):
-
         # Convert the 2D array to a DataFrame
         df = pd.DataFrame(matrix)
         #compute the correlation DataFrame
@@ -171,14 +92,21 @@ class ITPFExplainer(Explainer):
                 perturbed_matrix[:, i] *= (100 + (np.mean(perturbed_matrix[:, i]) * ((matrix.shape[1])/2) * (np.std(perturbed_matrix[:, i]))))
         
         return perturbed_matrix
-
     
     def _baseline(self, y):
+        """
+        Generate baseline pertubated sample from the input data
+
+        Parameters:
+        y (np.ndarray): Input data being explained (2D shape). Data used to create pertubated sample. 
+
+        Returns:
+        - perturbed sample.
+        """
         pertubed_samples = self._perturb_time_series(y)
 
         return pertubed_samples
     
-        
     def _compute_weight(self, total_features, subset_size):
         """
         Computes the weight for subsets of a given size in the context of Shapley values.
@@ -191,18 +119,33 @@ class ITPFExplainer(Explainer):
         - The weight for the subset.
         """
         return (math.factorial(subset_size - 1) * math.factorial(total_features - subset_size)) / math.factorial(total_features)
-   
-    
+
     def _create_mask(self, feature_num):
+        """
+        Generate and return the coalition matrix of size 2^feature_num
+        """
+        if(feature_num>12):
+            feature_num = 11
         mask = np.array(list(product(range(2), repeat=feature_num)))
         mask = mask[~np.all(mask == 0, axis=1)]
         mask = mask[~np.all(mask == 1, axis=1)]
     
         return mask
-    
-    
-    def _value_function(self, feature, feature_num, y, pertubed_data, model):
 
+    def _value_function(self, feature, feature_num, y, pertubed_data, model):
+        """
+        The function that computes and returns the average contribution of a feature.
+    
+        Parameters:
+        feature: The feature being analyzed.
+        feature_num: The number of features in the input data
+        y (np.ndarray): Input data being explained (2D shape).
+        pertubed_data: Perturbed data derived from y
+        model: model or prediction function of model takes 2D input and return 2D output.
+
+        Returns:
+        - The average contribution of "feature".
+        """
         # create coalition matrix for masking
         mask=self._create_mask(feature_num)
 
@@ -215,16 +158,13 @@ class ITPFExplainer(Explainer):
         baseline_tab = pertubed_data
         
         # compute baseline prediction
-        baseline_pred = model.predict_2Dto3D(baseline_tab)
+        #baseline_pred = model.predict_2Dto3D(baseline_tab)
+        baseline_pred = model(baseline_tab)
         # compute y_true
-        original_pred = model.predict_2Dto3D(data)
-
-        # reshap prediction 3d to 2d
-        baseline_pred_reshap = transform_to_2d(baseline_pred)
-        original_pred_reshap = transform_to_2d(original_pred)
+        original_pred = model(data)
 
         # compute RMSEvalue original_pred vs baseline_pres
-        rmse = mean_squared_error(original_pred_reshap, baseline_pred_reshap)
+        rmse = self.loss(original_pred, baseline_pred)
 
         # compute marginal contribution for baseline {feature} - {}
         weight = self._compute_weight(feature_num, 1)
@@ -253,28 +193,34 @@ class ITPFExplainer(Explainer):
         
 
             # compute marginal contribution
-            pred_without_feature = model.predict_2Dto3D(without_feature)
-            pred_with_feature = model.predict_2Dto3D(with_feature)
-
-            # reshap prediction 3d to 2d
-            pred_without_feature_reshap = transform_to_2d(pred_without_feature)
-            pred_with_feature_reshap = transform_to_2d(pred_with_feature)
+            pred_without_feature = model(without_feature)
+            pred_with_feature = model(with_feature)
 
             # compute rmse without_feature vs original_pred, rmse with_feature vs original_pred
-            rmse_without_feature = mean_squared_error(original_pred_reshap, pred_without_feature_reshap)
-            rmse_with_feature = mean_squared_error(original_pred_reshap, pred_with_feature_reshap)
-
+            rmse_without_feature = self.loss(original_pred, pred_without_feature)
+            rmse_with_feature = self.loss(original_pred, pred_with_feature)
+            # compute the marginal contribution of the feature in the combination
             pred = weight*(rmse_with_feature - rmse_without_feature)
             marginal_contribution.append(pred)
         
         # compute the average contribution
         average_contribution = np.mean(marginal_contribution)
         return average_contribution
+
+    def shap_values(self, y):
+        """
+        The function that computes and returns the average contribution of all features.
     
-    def shap_values_multivariate(self, y):
+        Parameters:
+        y (np.ndarray): Input data being explained (timesteps, features) (2D shape).
+        
+        Returns:
+        - The average contribution of each features (np.arrays)".
+        - The baseline perturbed sample
+        """
         # step 1: configuration
         pertubed_data = self._perturb_time_series(y)
-        n_features = self.feature_num
+        n_features = y.shape[1]
         shapley_values = []
         
         # step 2: Compute the shapeley values for each feature
@@ -284,7 +230,10 @@ class ITPFExplainer(Explainer):
             shapley_values.append(s_value)
         return shapley_values, pertubed_data
     
-    def shap_multivariate(self, s_values):
+    def plot_shap_values(self, s_values):
+        """
+        Plot the features average contribution with SHAP value
+        """
         shap_values = s_values
         feature_names = self.feature_names
 
@@ -297,20 +246,5 @@ class ITPFExplainer(Explainer):
         plt.barh(y_pos, shap_values, align='center', color=bar_colors)
         plt.yticks(y_pos, feature_names)
         plt.xlabel('Mean SHAP Value')
-        plt.title('Feature Importance with SHAP Values')
+        plt.title('Feature Average Contribution with SHAP Values')
         plt.show()
-
-## Gaussian process
-## Prediction uncertainity
-## Prediction interval
-    
-## Contribution of individual covariate:
-    # - Shapley value
-
-## Partial Dependence - Link function shape
-    
-#def predict(self, X):
-    # here the input is 2-d and we want to transform it to 3-d before prediction
-#    x = X.reshape((X.shape[0], X.shape[1], 1))
-#    y = self.clf.predict(x)
-#    return y
